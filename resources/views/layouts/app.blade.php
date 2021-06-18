9ddc1543813d2255ba028fbece2821c635a41c09
Henry Warui <warui.henry@gmail.com>
2021-06-18 18:30:21 +0300

    Laracast voting app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title> Laravel Voting </title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans text-gray-900 text-sm">
+    <header class="flex items-center justify-between px-8 py-4">
+     <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="Logo"></a>
+     <div class="flex items-center">
+     @if (Route::has('login'))
+                <div class=" px-6 py-4 ">
+                    @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link>
+                            <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }} 
+                                </a>
+                            </x-dropdown-link>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+     <a href="#+">
+     <img src="https://gravatar.com/avatar/00000000000000000000000000000000/?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+     </a>
+     </div>
+     </header>
+    </body>
+</html>
