1d4eee3b47e9ea253fd0a0905f3e01668396aede
Henry Warui <warui.henry@gmail.com>
2021-06-21 11:25:00 +0300

    initial setup and design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,31 @@
 <x-app-layout>
-    <div>Index of Ideas</div>
+  <div class="filters flex space-x-6">
+    <div class="w-1/3">
+      <select name="category" id="category" class="w-full rounded-xl border-none py-2 px-4">
+        <option value="categoryOne">Category One</option>
+        <option value="categoryTwo">Category Two</option>
+        <option value="categoryThree">Category Three</option>
+        <option value="categoryFour">Category Four</option>
+     </select>
+    </div>
+
+     <div class="w-1/3">
+      <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none py-2 px-4">
+        <option value="filter one ">filter  One</option>
+        <option value="filter Two">filter Two</option>
+        <option value="filter Three">filter Three</option>
+        <option value="filter four">filter Four</option>
+     </select>
+    </div>
+
+    <div class="w-2/3 relative">
+        <input type="search" class="w-full rounded-xl border-none placeholder-text-gray-900  px-4 py-2 pl-8 " placeholder="Find an idea">
+        <div class="absolute top-0 items-center h-full flex ml-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+        </div>
+    </div>
+
+  </div> <!-- end filters -->
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title> Laravel Voting </title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
+    <title> Laravel Voting </title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans text-gray-900 text-sm">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans bg-gray-background text-gray-900 text-sm">
+
     <header class="flex items-center justify-between px-8 py-4">
-     <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="Logo"></a>
-     <div class="flex items-center">
-     @if (Route::has('login'))
+        <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="Logo"></a>
+        <div class="flex items-center">
+            @if (Route::has('login'))
                 <div class=" px-6 py-4 ">
                     @auth
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
                             <x-dropdown-link>
-                            <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }} 
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                                this.closest('form').submit();">
+                                    {{ __('Log Out') }}
                                 </a>
                             </x-dropdown-link>
                         </form>
@@ -43,10 +45,45 @@
                     @endauth
                 </div>
             @endif
-     <a href="#+">
-     <img src="https://gravatar.com/avatar/00000000000000000000000000000000/?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
-     </a>
-     </div>
-     </header>
-    </body>
+            <a href="#+">
+                <img src="https://gravatar.com/avatar/00000000000000000000000000000000/?d=mp" alt="avatar"
+                    class="w-10 h-10 rounded-full">
+            </a>
+        </div>
+    </header>
+
+    <main class="container mx-auto max-width-custom flex">
+
+        <div class="w-70 mr-5 ml-40">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos facilis quisquam quis, nulla, quibusdam
+            officia dolorum cupiditate similique tenetur sit consectetur illo cumque. Eos laudantium eligendi labore
+            reiciendis ex necessitatibus.
+        </div>
+
+        <div class='w-175'>
+            <nav class="flex items-center justify-between text-xs">
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li><a href="#" class="border-b-4 pb-3 border-blue">All ideas (87)</a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering
+                        (6)</a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In
+                        progress (10)</a></li>
+                </ul>
+
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10) </a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed
+                        (55) </a></li>
+                </ul>
+            </nav>
+           <div class="mt-8">{{ $slot }}</div>
+        
+        </div>
+    </main>
+</body>
+
 </html>
